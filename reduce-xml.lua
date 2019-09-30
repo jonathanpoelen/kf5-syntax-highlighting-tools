37ece0f0b10a00789ad53e153a27231d590e0614
Jonathan Poelen <jonathan.poelen@gmail.com>
2019-10-01 01:55:33 +0200

    reduce-xml: replace true/false by 1/0 and remove some unnecessary attributes

--- reduce-xml.lua
+++ reduce-xml.lua
@@ -7,16 +7,44 @@ end
 
 lpeg = require'lpeg'
 local P = lpeg.P
-local C = lpeg.C
 local S = lpeg.S
-local Cf = lpeg.Cf
-local Cc = lpeg.Cc
-local Cs = lpeg.Cs
-local Ct = lpeg.Ct
 
-local endcomment = P'-->'
+local After = function(p) p=P(p) return (1 - p)^0 * p end
+
 local ws = S'\t \n'
-local reduce = Cs(((ws^1 * '<') / '<' * (('!--' * (1-endcomment)^0 * endcomment) / '')^-1 + 1)^0 + ws^0 / '')
+local ws0 = ws^0
+local ws1 = ws^1
+local ws0r = ws0/''
+local ws1r = ws1/''
+local str = '"' * After'"'
+local ws1s = ws1 / ' ' + str
+local noclose = (1 - S'>')
+-- TODO remove attribute with lookAhead=1
+-- TODO remove attribute if same as context attribute
+local reduce = lpeg.Cs(ws0r *
+  ( '<' *
+    ( '!--' * After'-->' / ''
+    + '!' * (ws1s + (1 - S'>['))^0 *
+      ( S'>'
+      + S'['
+        * (ws0r * '<' * (ws1s + noclose)^1 * '>')^0
+        * ws0r
+      )
+    + ( ws0 * '/' / '/'
+      + ws0 * (P'fallthrough="' * (P'true' + '1') * '"' + P'context="#stay"') / ''
+      + ws1s
+      + (P'lookAhead' + 'casesensitive' + 'firstNonSpace'
+         +'insensitive' + 'spellChecking' + 'dynamic'
+         +'bold' + 'italic' + 'underline'
+        )
+        * '="' * (P'true' / '1' + P'false' / '0') * '"'
+      + noclose
+      )^1
+    ) * ws0r
+  + ws1r
+  + 1
+  )^0
+)
 
 local prefix = arg[1]
 local suffix = arg[2]
