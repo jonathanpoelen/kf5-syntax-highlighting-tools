62bb29d736ea83d198b510b30ad40e5818906d5f
Jonathan Poelen <jonathan.poelen@gmail.com>
2019-10-04 02:35:34 +0200

    remove casesensitive (WIP)

--- reduce-xml.lua
+++ reduce-xml.lua
@@ -9,6 +9,7 @@ lpeg = require'lpeg'
 local P = lpeg.P
 local S = lpeg.S
 local R = lpeg.R
+local Cp = lpeg.Cp()
 
 local After = function(p) p=P(p) return (1 - p)^0 * p end
 
@@ -27,8 +28,28 @@ local argsdoctype = (ws1r * (str + word))^1 * ws0r
 
 local VAttr = function(p) p=P(p) return eq * ('"' * p * '"' + "'" * p * "'") end
 
+local attrIgnoreFalse
+=P'lookAhead' + 'firstNonSpace' + 'dynamic'
++ 'bold' + 'italic' + 'underline'
++ 'spellChecking'
++ 'indentationsensitive'
+
+local attrIgnoreTrue
+=P'fallthrough'
++ 'casesensitive'
+
+local attrBool
+= attrIgnoreFalse + attrIgnoreTrue
++ 'insensitive'
+
+local state_casesensitive
+function reset_state() 
+  state_casesensitive = true
+end
+
 -- TODO remove attribute with lookAhead=1
 -- TODO remove attribute if same as context attribute
+-- TODO casensitive if same as global casesensitive
 local reduce = lpeg.Cs(
   P'\xEF\xBB\xBF'^-1 / '' -- BOM
 
@@ -40,32 +61,30 @@ local reduce = lpeg.Cs(
   * '>'
   )^-1
 
-* ( blank
-  * '<'
+* ( '<'
   * ( '/' * word
     + word
     * ( ws0
-        * ( 'fallthrough' * VAttr(P'true' + '1')
-          + 'context' * VAttr('#stay')
-          + 'indentationsensitive' * VAttr(P'0' + 'false')
-          ) / ''
+      * ( attrIgnoreTrue * VAttr(P'true' + '1')
+        + 'context' * VAttr('#stay')
+        + 'weakDeliminator' * eq * (P'""' + "''")
+        + attrIgnoreFalse * VAttr(P'0' + 'false')
+        ) / ''
       + ws1r
-        * ( ( P'lookAhead' + 'casesensitive' + 'firstNonSpace'
-            + 'insensitive' + 'spellChecking' + 'dynamic'
-            + 'bold' + 'italic' + 'underline'
-            + 'indentationsensitive'
-            )
-            * VAttr(P'true' / '1' + P'false' / '0')
-          + attr
-          )
+      * ( 'casesensitive' * VAttr(P'false' / '0' + '0') * (Cp/function() state_casesensitive = false end)
+        + attrBool * VAttr(P'true' / '1' + P'false' / '0')
+        + attr
+        )
       )^0
     * ws0r
     * P'/'^-1
     )
   * '>'
+  + blank
   + 1
   )^0
 )
+
 
 local prefix = arg[1]
 local suffix = arg[2]
@@ -76,6 +95,8 @@ for _,filename in ipairs({table.unpack(arg, 3)}) do
   content = f:read('*a')
   f:close()
   f = io.open(prefix .. filename .. suffix, 'w')
+  reset_state()
   f:write(reduce:match(content))
+  print(state_casesensitive)
   f:close()
 end
