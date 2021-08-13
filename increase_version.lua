6a3afefb7367c4be7b4e7bed538fc7f1e9cfde1e
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-08-13 21:54:05 +0200

    fix increase_versio.lua when the version is preceded by tabs

--- increase_version.lua
+++ increase_version.lua
@@ -1,13 +1,13 @@
 #!/usr/bin/env lua 
-local f, s
+local f, s, err
 for _,filename in ipairs(arg) do
   print(filename)
   f,err = io.open(filename)
   if err then
     io.stderr:write(err..'\n')
   else
-    s = f:read'*a':gsub(' version="(%d+)"', function(n)
-      return ' version="' .. tonumber(n)+1 .. '"'
+    s = f:read'*a':gsub('([ \t])version="(%d+)"', function(sp, n)
+      return sp .. 'version="' .. tonumber(n)+1 .. '"'
     end)
     f:close()
     f = io.open(filename, 'w+')
